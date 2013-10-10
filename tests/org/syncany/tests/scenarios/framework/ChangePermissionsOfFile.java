10939460112e6c8e6f846aa2b262b413aabf8eae
Philipp Heckel <philipp.heckel@gmail.com>
2013-10-10 17:35:34 +0200

    MINOR: Added ChangeFilePermissions* tests

---
+++ tests/org/syncany/tests/scenarios/framework/ChangePermissionsOfFile.java
@@ -0,0 +1,28 @@
+package org.syncany.tests.scenarios.framework;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
+
+import org.syncany.util.FileUtil;
+
+public class ChangePermissionsOfFile extends AbstractClientAction {
+	@Override
+	public void execute() throws Exception {
+		File file = pickFile(hashCode());
+		Path filePath = Paths.get(file.getAbsolutePath());
+				
+		if (FileUtil.isWindows()) {
+			Files.setAttribute(filePath, "dos:hidden", true);
+			Files.setAttribute(filePath, "dos:system", true);
+			Files.setAttribute(filePath, "dos:readonly", true);
+			Files.setAttribute(filePath, "dos:archive", true);
+		}
+		else if (FileUtil.isUnixLikeOperatingSystem()) {
+			log(this, "rwxrwxrwx "+file.getAbsolutePath());
+			Files.setPosixFilePermissions(filePath, PosixFilePermissions.fromString("rwxrwxrwx"));
+		}		
+	}		
+}	
---
+++ tests/org/syncany/tests/scenarios/framework/ChangePermissionsOfFolder.java
@@ -0,0 +1,28 @@
+package org.syncany.tests.scenarios.framework;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
+
+import org.syncany.util.FileUtil;
+
+public class ChangePermissionsOfFolder extends AbstractClientAction {
+	@Override
+	public void execute() throws Exception {
+		File file = pickFolder(hashCode());
+		Path filePath = Paths.get(file.getAbsolutePath());
+				
+		if (FileUtil.isWindows()) {
+			Files.setAttribute(filePath, "dos:hidden", true);
+			Files.setAttribute(filePath, "dos:system", true);
+			Files.setAttribute(filePath, "dos:readonly", true);
+			Files.setAttribute(filePath, "dos:archive", true);
+		}
+		else if (FileUtil.isUnixLikeOperatingSystem()) {
+			log(this, "rwxrwxrwx "+file.getAbsolutePath());
+			Files.setPosixFilePermissions(filePath, PosixFilePermissions.fromString("rwxrwxrwx"));
+		}		
+	}		
+}	
