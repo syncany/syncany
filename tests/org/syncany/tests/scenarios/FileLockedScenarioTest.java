0798071b21af093e034560441eb9044c8cc1de56
Philipp Heckel <philipp.heckel@gmail.com>
2013-10-14 01:28:36 +0200

    MINOR: Fix imports of file locked test

--- tests/org/syncany/tests/scenarios/FileLockedScenarioTest.java
+++ tests/org/syncany/tests/scenarios/FileLockedScenarioTest.java
@@ -13,7 +13,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
 import java.util.HashSet;
 import java.util.Set;
 
