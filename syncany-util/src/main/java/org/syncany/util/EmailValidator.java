54e3713acd5a357dd3dfa89799fffcfeb532e07a
Vincent Wiencek <vwiencek@gmail.com>
2013-12-24 11:08:06 +0100

    Adding EmailValidator + test class

---
+++ syncany-util/src/main/java/org/syncany/util/EmailValidator.java
@@ -0,0 +1,51 @@
+/*
+ * Syncany, www.syncany.org
+ * Copyright (C) 2011-2013 Philipp C. Heckel <philipp.heckel@gmail.com> 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.syncany.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Vincent Wiencek <vwiencek@gmail.com>
+ *
+ */
+public class EmailValidator {
+	private Pattern pattern;
+	private Matcher matcher;
+ 
+	private static final String EMAIL_PATTERN = 
+		  "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*" 
+		+ "@"
+		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+ 
+	public EmailValidator() {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+	}
+ 
+	/**
+	 * Validate hex with regular expression
+	 * 
+	 * @param email
+	 *            email to be validated
+	 * @return true valid email, false invalid email
+	 */
+	public boolean validate(final String email) {
+		matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+}
