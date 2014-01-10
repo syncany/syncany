81562de3e664cb893853e09088f131114f588eac
Pim Otte <otte.pim@gmail.com>
2014-01-10 11:22:38 +0100

    S3 plugin

--- syncany-plugins/syncany-plugin-rest/src/main/java/org/syncany/connection/plugins/s3/S3Connection.java
+++ syncany-plugins/syncany-plugin-rest/src/main/java/org/syncany/connection/plugins/s3/S3Connection.java
@@ -17,11 +17,12 @@
  */
 package org.syncany.connection.plugins.s3;
 
-import java.util.List;
 import java.util.Map;
 
 import org.jets3t.service.security.AWSCredentials;
 import org.jets3t.service.security.ProviderCredentials;
+import org.syncany.connection.plugins.PluginSetting;
+import org.syncany.connection.plugins.PluginSetting.ValueType;
 import org.syncany.connection.plugins.StorageException;
 import org.syncany.connection.plugins.TransferManager;
 import org.syncany.connection.plugins.rest.RestConnection;
@@ -29,17 +30,14 @@ import org.syncany.connection.plugins.rest.RestConnection;
 public class S3Connection extends RestConnection {   
     // cp. http://jets3t.s3.amazonaws.com/api/constant-values.html#org.jets3t.service.model.S3Bucket.LOCATION_ASIA_PACIFIC
     private String location;
+    Map<String, PluginSetting> settings = null;
 
     @Override
-	public void init(Map<String, String> map) throws StorageException {
-    	super.init(map);
-    	
+	public void init() {
+    	super.init();
+    	Map<String, PluginSetting> map = getSettings();
     	// Additional S3 settings
-		location = map.get("location");
-		
-		if (location == null) {
-			throw new StorageException("Config does not contain 'location' setting.");
-		}
+		location = map.get("location").getValue();
 	}
 
     @Override
@@ -58,6 +56,15 @@ public class S3Connection extends RestConnection {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+    
+    @Override
+    public Map<String, PluginSetting> getSettings() {
+    	if (settings == null) {
+    		settings = super.getSettings();
+    		settings.put("location", new PluginSetting(ValueType.STRING, true, false));
+    	}
+    	return settings;
     }
     
 }
