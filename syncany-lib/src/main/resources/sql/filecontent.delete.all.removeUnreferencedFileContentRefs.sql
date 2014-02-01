3e6cd7be17314700d10501d18e6ef2dae92a9baa
Pim Otte <otte.pim@gmail.com>
2014-02-01 17:01:03 +0100

    removing minus clause for Unreferenced File queries

--- syncany-lib/src/main/resources/sql/filecontent.delete.all.removeUnreferencedFileContentRefs.sql
+++ syncany-lib/src/main/resources/sql/filecontent.delete.all.removeUnreferencedFileContentRefs.sql
@@ -2,12 +2,7 @@
 -- that are not used by any file versions anymore
 
 delete from filecontent_chunk
-where filecontent_checksum in (
-	select distinct checksum
-	from filecontent
-
-		minus
-	
+where filecontent_checksum not in (
 	select distinct filecontent_checksum
 	from fileversion
 )
