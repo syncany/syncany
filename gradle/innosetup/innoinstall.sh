91f25667527a95f1278f6e9b7586c8ee7789931e
Philipp Heckel <philipp.heckel@gmail.com>
2014-02-22 01:43:15 +0100

    Add innoextract from PPA (pre-Ubuntu 12.10)

--- gradle/innosetup/innoinstall.sh
+++ gradle/innosetup/innoinstall.sh
@@ -6,6 +6,9 @@ cd /tmp/inno
 
 wget -O is.exe http://www.jrsoftware.org/download.php/is.exe
 innoextract is.exe
-mkdir ~/".wine/drive_c/Program Files (x86)/Inno Setup 5"
-cp -a app/* ~/".wine/drive_c/Program Files (x86)/Inno Setup 5"
+mkdir -p ~/".wine/drive_c/inno"
+cp -a app/* ~/".wine/drive_c/inno"
+
+#mkdir ~/".wine/drive_c/Program Files (x86)/Inno Setup 5"
+#cp -a app/* ~/".wine/drive_c/Program Files (x86)/Inno Setup 5"
 
