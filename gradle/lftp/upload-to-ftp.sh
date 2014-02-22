6dc23feaedb23a44ea3f06cc1be10c1d2ba6dccf
Philipp Heckel <philipp.heckel@gmail.com>
2014-02-22 02:35:56 +0100

    Cont'd Travis Inno Setup config

--- gradle/lftp/upload-to-ftp.sh
+++ gradle/lftp/upload-to-ftp.sh
@@ -5,7 +5,6 @@ REPODIR="$SCRIPTDIR/../.."
 
 TEMPDIR="$SCRIPTDIR/temp"
 TEMPDISTDIR="$TEMPDIR/dist"
-TEMPJAVADOCDIR="$TEMPDIR/javadoc"
 
 if [ "$SYNCANY_FTP_HOST" == "" -o "$SYNCANY_FTP_USER" == "" -o "$SYNCANY_FTP_PASS" == "" ]; then
 	echo "ERROR: SYNCANY_FTP_* environment variables not set."
@@ -15,14 +14,14 @@ fi
 # Reset tempdir
 rm -rf "$TEMPDIR"
 mkdir $TEMPDIR
-mkdir $TEMPDIR/dist
-mkdir $TEMPDIR/javadoc
+mkdir $TEMPDISTDIR
 
 # Gather deb/tar-gz/zip
 cp $REPODIR/syncany-cli/build/distributions/*.{zip,tar.gz} $TEMPDISTDIR
 cp $REPODIR/syncany-cli/build/linux-package/*.deb $TEMPDISTDIR
+cp $REPODIR/syncany-cli/build/innosetup/*.exe $TEMPDISTDIR
 
-if [ $(ls $TEMPDISTDIR | wc -l) != "3" ]; then
+if [ $(ls $TEMPDISTDIR | wc -l) != "4" ]; then
 	echo "ERROR: Wrong files in $TEMPDISTDIR: "
 	ls $TEMPDISTDIR
 	
@@ -30,14 +29,19 @@ if [ $(ls $TEMPDISTDIR | wc -l) != "3" ]; then
 fi
 
 # Gather JavaDoc
-cp -a $REPODIR/build/javadoc/* $TEMPJAVADOCDIR/
+PWD=`pwd`
+cd $REPODIR/build
+rm javadoc.tar.gz
+tar -czf javadoc.tar.gz javadoc/
+mv javadoc.tar.gz $TEMPDIR/
+cd "$PWD"
 
 # Copy to FTP 
 echo "Uploading files to Syncany FTP ..."
 
 lftp -c "open ftp://$SYNCANY_FTP_HOST
 user $SYNCANY_FTP_USER $SYNCANY_FTP_PASS
-mirror --reverse --only-newer --delete --parallel=2 --verbose $TEMPDIR /
+mirror --reverse --only-newer --delete --parallel=3 --verbose $TEMPDIR /
 bye
 "
 
