ea39d39962bd96c07352959f59f9176bc72d4d58
Philipp Heckel <philipp.heckel@gmail.com>
2014-02-22 01:22:35 +0100

    Inno Setup for Travis CI; first test

---
+++ gradle/innosetup/innoinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf /tmp/inno
+mkdir /tmp/inno
+cd /tmp/inno
+
+wget -O is.exe http://www.jrsoftware.org/download.php/is.exe
+innoextract is.exe
+mkdir ~/".wine/drive_c/Program Files (x86)/Inno Setup 5"
+cp -a app/* ~/".wine/drive_c/Program Files (x86)/Inno Setup 5"
+
