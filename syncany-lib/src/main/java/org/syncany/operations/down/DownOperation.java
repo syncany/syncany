2e08390855ecbb5bd6e774d2e45e82ec5a635d82
Philipp Heckel <philipp.heckel@gmail.com>
2014-05-23 22:14:32 +0200

    Split off ApplyChangesOperation from DownOperation; trying to make it easier to understand

--- syncany-lib/src/main/java/org/syncany/operations/down/DownOperation.java
+++ syncany-lib/src/main/java/org/syncany/operations/down/DownOperation.java
@@ -18,15 +18,10 @@
 package org.syncany.operations.down;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -37,18 +32,14 @@ import java.util.logging.Logger;
 
 import org.syncany.config.Config;
 import org.syncany.connection.plugins.DatabaseRemoteFile;
-import org.syncany.connection.plugins.MultiChunkRemoteFile;
 import org.syncany.connection.plugins.StorageException;
 import org.syncany.connection.plugins.TransferManager;
-import org.syncany.database.ChunkEntry.ChunkChecksum;
 import org.syncany.database.DatabaseVersion;
 import org.syncany.database.DatabaseVersionHeader;
 import org.syncany.database.DatabaseVersionHeader.DatabaseVersionType;
-import org.syncany.database.FileContent;
 import org.syncany.database.FileVersion;
 import org.syncany.database.MemoryDatabase;
 import org.syncany.database.MultiChunkEntry;
-import org.syncany.database.MultiChunkEntry.MultiChunkId;
 import org.syncany.database.PartialFileHistory;
 import org.syncany.database.PartialFileHistory.FileHistoryId;
 import org.syncany.database.SqlDatabase;
@@ -59,13 +50,10 @@ import org.syncany.operations.AbstractTransferOperation;
 import org.syncany.operations.cleanup.CleanupOperation;
 import org.syncany.operations.down.DownOperationOptions.DownConflictStrategy;
 import org.syncany.operations.down.DownOperationResult.DownResultCode;
-import org.syncany.operations.down.actions.FileCreatingFileSystemAction;
 import org.syncany.operations.down.actions.FileSystemAction;
-import org.syncany.operations.down.actions.FileSystemAction.InconsistentFileSystemException;
 import org.syncany.operations.ls_remote.LsRemoteOperation;
 import org.syncany.operations.ls_remote.LsRemoteOperation.LsRemoteOperationResult;
 import org.syncany.operations.up.UpOperation;
-import org.syncany.util.FileUtil;
 
 import com.google.common.collect.Sets;
 import com.google.common.collect.Sets.SetView;
@@ -87,13 +75,8 @@ import com.google.common.collect.Sets.SetView;
  *  <li>Determine whether the local branch conflicts with the winner branch; if so, prune conflicting
  *      local database versions (using {@link DatabaseReconciliator#findLosersPruneBranch(DatabaseBranch, DatabaseBranch)
  *      findLosersPruneBranch()})</li>
- *  <li>Determine whether the local branch needs to be updated (new database versions); if so, determine
- *      local {@link FileSystemAction}s</li>
- *  <li>Determine, download and decrypt required multi chunks from remote storage from file actions
- *      (implemented in {@link #determineMultiChunksToDownload(FileVersion, MemoryDatabase, MemoryDatabase) determineMultiChunksToDownload()},
- *      and {@link #downloadAndDecryptMultiChunks(Set) downloadAndDecryptMultiChunks()})</li>
- *  <li>Apply file system actions locally, creating conflict files where necessary if local file does
- *      not match the expected file (implemented in {@link #applyFileSystemActions(List) applyFileSystemActions()} </li>
+ *  <li>If the apply-changes-flag is switched on, changes are applied to the local file system using the
+ *      {@link ApplyChangesOperation}.</li>
  *  <li>Save local database and update known database list (database files that do not need to be 
  *      downloaded anymore</li>  
  * </ol>
@@ -389,14 +372,7 @@ public class DownOperation extends AbstractTransferOperation {
 			MemoryDatabase winnersDatabase = readWinnersDatabase(winnersApplyBranch, databaseFileList, DatabaseVersionType.DEFAULT, ignoredMostRecentPurgeVersions);
 			
 			if (options.isApplyChanges()) {
-				logger.log(Level.INFO, "Determine file system actions ...");			
-				FileSystemActionReconciliator actionReconciliator = new FileSystemActionReconciliator(config, result);
-				List<FileSystemAction> actions = actionReconciliator.determineFileSystemActions(winnersDatabase);
-	
-				Set<MultiChunkId> unknownMultiChunks = determineRequiredMultiChunks(actions, winnersDatabase);
-				downloadAndDecryptMultiChunks(unknownMultiChunks);
-	
-				applyFileSystemActions(actions);
+				new ApplyChangesOperation(config, localDatabase, transferManager, winnersDatabase, result).execute();
 			}
 			else {
 				logger.log(Level.INFO, "Doing nothing on the file system, because --no-apply switched on");			
@@ -520,131 +496,6 @@ public class DownOperation extends AbstractTransferOperation {
 		return mostRecentPurgeVersions;
 	}
 	
-	/**
-	 * Finds the multichunks that need to be downloaded to apply the given file system actions.
-	 * The method looks at all {@link FileCreatingFileSystemAction}s and returns their multichunks. 
-	 */
-	private Set<MultiChunkId> determineRequiredMultiChunks(List<FileSystemAction> actions, MemoryDatabase winnersDatabase) {
-		Set<MultiChunkId> multiChunksToDownload = new HashSet<MultiChunkId>();
-
-		for (FileSystemAction action : actions) {
-			if (action instanceof FileCreatingFileSystemAction) { // TODO [low] This adds ALL multichunks even though some might be available locally
-				multiChunksToDownload.addAll(determineMultiChunksToDownload(action.getFile2(), winnersDatabase));
-			}
-		}
-
-		return multiChunksToDownload;
-	}
-	
-	/**
-	 * Finds the multichunks that need to be downloaded for the given file version -- using the local 
-	 * database and given winners database. Returns a set of multichunk identifiers.
-	 */
-	private Collection<MultiChunkId> determineMultiChunksToDownload(FileVersion fileVersion, MemoryDatabase winnersDatabase) {
-		Set<MultiChunkId> multiChunksToDownload = new HashSet<MultiChunkId>();
-
-		// First: Check if we know this file locally!
-		List<MultiChunkId> multiChunkIds = localDatabase.getMultiChunkIds(fileVersion.getChecksum());
-		
-		if (multiChunkIds.size() > 0) {
-			multiChunksToDownload.addAll(multiChunkIds);
-		}
-		else {
-			// Second: We don't know it locally; must be from the winners database
-			FileContent winningFileContent = winnersDatabase.getContent(fileVersion.getChecksum());			
-			boolean winningFileHasContent = winningFileContent != null;
-
-			if (winningFileHasContent) { // File can be empty!
-				List<ChunkChecksum> fileChunks = winningFileContent.getChunks(); 
-				
-				// TODO [medium] Instead of just looking for multichunks to download here, we should look for chunks in local files as well
-				// and return the chunk positions in the local files ChunkPosition (chunk123 at file12, offset 200, size 250)
-				
-				Map<ChunkChecksum, MultiChunkId> checksumsWithMultiChunkIds = localDatabase.getMultiChunkIdsByChecksums(fileChunks);
-				
-				for (ChunkChecksum chunkChecksum : fileChunks) {
-					MultiChunkId multiChunkIdForChunk = checksumsWithMultiChunkIds.get(chunkChecksum);
-					if (multiChunkIdForChunk == null) {
-						multiChunkIdForChunk = winnersDatabase.getMultiChunkIdForChunk(chunkChecksum);
-						
-						if (multiChunkIdForChunk == null) {
-							throw new RuntimeException("Cannot find multichunk for chunk "+chunkChecksum);	
-						}
-					}
-					
-					if (!multiChunksToDownload.contains(multiChunkIdForChunk)) {
-						logger.log(Level.INFO, "  + Adding multichunk " + multiChunkIdForChunk + " to download list ...");
-						multiChunksToDownload.add(multiChunkIdForChunk);
-					}
-				}
-			}
-		}
-		
-		return multiChunksToDownload;
-	}
-	
-	/** 
-	 * Downloads the given multichunks from the remote storage and decrypts them
-	 * to the local cache folder. 
-	 */
-	private void downloadAndDecryptMultiChunks(Set<MultiChunkId> unknownMultiChunkIds) throws StorageException, IOException {
-		logger.log(Level.INFO, "Downloading and extracting multichunks ...");
-
-		// TODO [medium] Check existing files by checksum and do NOT download them if they exist locally, or copy them
-
-		for (MultiChunkId multiChunkId : unknownMultiChunkIds) {
-			File localEncryptedMultiChunkFile = config.getCache().getEncryptedMultiChunkFile(multiChunkId);
-			File localDecryptedMultiChunkFile = config.getCache().getDecryptedMultiChunkFile(multiChunkId);
-			MultiChunkRemoteFile remoteMultiChunkFile = new MultiChunkRemoteFile(multiChunkId);
-
-			logger.log(Level.INFO, "  + Downloading multichunk " + multiChunkId + " ...");
-			transferManager.download(remoteMultiChunkFile, localEncryptedMultiChunkFile);
-			result.getDownloadedMultiChunks().add(multiChunkId);
-
-			logger.log(Level.INFO, "  + Decrypting multichunk " + multiChunkId + " ...");
-			InputStream multiChunkInputStream = config.getTransformer().createInputStream(new FileInputStream(localEncryptedMultiChunkFile));
-			OutputStream decryptedMultiChunkOutputStream = new FileOutputStream(localDecryptedMultiChunkFile);
-
-			// TODO [medium] Calculate checksum while writing file, to verify correct content
-			FileUtil.appendToOutputStream(multiChunkInputStream, decryptedMultiChunkOutputStream);
-
-			decryptedMultiChunkOutputStream.close();
-			multiChunkInputStream.close();
-
-			logger.log(Level.FINE, "  + Locally deleting multichunk " + multiChunkId + " ...");
-			localEncryptedMultiChunkFile.delete();
-		}
-
-		transferManager.disconnect();
-	}
-	
-	/**
-	 * Applies the given file system actions in a sensible order. To do that, 
-	 * the given actions are first sorted using the {@link FileSystemActionComparator} and
-	 * then executed individually using {@link FileSystemAction#execute()}.
-	 */
-	private void applyFileSystemActions(List<FileSystemAction> actions) throws Exception {
-		// Sort
-		FileSystemActionComparator actionComparator = new FileSystemActionComparator();
-		actionComparator.sort(actions);
-
-		logger.log(Level.FINER, "- Applying file system actions (sorted!) ...");
-
-		// Apply
-		for (FileSystemAction action : actions) {
-			if (logger.isLoggable(Level.FINER)) {
-				logger.log(Level.FINER, "   +  {0}", action);
-			}
-
-			try {
-				action.execute();
-			}
-			catch (InconsistentFileSystemException e) {
-				logger.log(Level.FINER, "     --> Inconsistent file system exception thrown. Ignoring for this file.", e);
-			}
-		}
-	}
-
 	/**
 	 * Persists the given winners branch to the local database, i.e. for every database version
 	 * in the winners branch, all contained multichunks, chunks, etc. are added to the local SQL 
