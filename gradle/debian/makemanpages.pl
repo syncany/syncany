4612f5dba1257264f981ae3001a4fed005ba8bff
Philipp Heckel <philipp.heckel@gmail.com>
2014-06-13 08:26:06 +0200

    Man pages perl script; first working converter of help pages

--- gradle/debian/makemanpages.pl
+++ gradle/debian/makemanpages.pl
@@ -25,74 +25,91 @@ foreach (@manpages) {
 
 	my $indent = 0;
 	my $nf = 0;
+	my $indentSection = 0;
+	my @indents = (0);
 
 	while (my $line = <IN>) {
 		chomp $line;
 	
 		# Trim right
 		$line =~ s/\s+$//g; 
+		
+		# Escape line 
+		$line =~ s/\\/\\\\/g;
 	
 		# Header
-		if ($line =~ /^[A-Z\s]+$/) {	
+		if ($line =~ /^([A-Z\s]+)$/) {	
 			if ($indent > 0) {
 				print OUT ".RE\n";
 				$indent = 0;
 			}
+			
+			if ($1 =~ /SYNOPSIS|DESCRIPTION/i) {
+				$indentSection = 0;
+			}
+			else {
+				$indentSection = 1;
+			}
 		
 			print OUT ".SH $line\n";
-			$lastth = $line;
 		}
 	
 		# Empty line
 		elsif ($line =~ /^\s*$/) {
 			print OUT ".PP\n";
-			#print OUT "\n";
 		}
+
+		# Indent section		
 		else {	
 			# Trim left two spaces
 			$line =~ s/^\s\s//g; 
 		
-			# Calculate indent (after two left spaces)
-			my $lineindent = 0;
+			if ($indentSection) {
+				# Calculate indent (after two left spaces)
+				my $lineindent = 0;
 	
-			if ($line =~ /^(\s+)/) {
-				$lineindent = length $1;
-			}
+				if ($line =~ /^(\s+)/) {
+					$lineindent = length $1;
+				}
 		
+				# Don't break list items and option lines
+				if (!$nf && $line =~ /^\s*[-\*]/) {
+					print OUT ".nf\n";
+					$nf = 1;
+				}
+				elsif ($nf && $line =~ /^\s*[^-\*]/) {
+					print OUT ".fi\n";
+					$nf = 0;
+				}
 		
-			if (!$nf && $line =~ /^\s*[-\*]/) {
-				print OUT ".nf\n";
-				$nf = 1;
-			}
-			elsif ($nf && $line =~ /^\s*[^-\*]/) {
-				print OUT ".fi\n";
-				$nf = 0;
-			}
+				# Remove indent 
+				$line =~ s/^\s*//g; 
 
-		
-			# Remove indent 
-			$line =~ s/^\s*//g; 
-
-			# Print without indent
-			if ($lineindent > $indent) {
-				print OUT ".RS\n";
-				print OUT "$line\n";
-			}
-			elsif ($lineindent < $indent) {
-				print OUT ".RE\n";
-				print OUT "$line\n";
+				# End indent sections				
+				while ($lineindent < $indent && $#indents > 0) {
+					$indent = pop(@indents);
+					print OUT ".RE\n";				
+				}
+				
+				if ($lineindent > $indent) { 
+					print OUT ".RS\n";
+					print OUT "$line\n";
+					
+					push(@indents, $indent);
+					$indent = $lineindent;
+				}
+				else {												
+					print OUT "$line\n";
+					$indent = $lineindent;
+				}
 			}
 			else {
 				print OUT "$line\n";
 			}
-	
-			# Set new indent	
-			$indent = $lineindent
 		}
 	}
 
 	close(IN);
 	close(OUT);
-
 }
 
