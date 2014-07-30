6d373e45ff5874b378e89aa0269d58c238ffd944
Pim Otte <otte.pim@gmail.com>
2014-07-30 21:31:10 +0200

    Reflecting to obtain a test that installs the ftp plugin

--- syncany-lib/src/test/java/org/syncany/tests/operations/PluginOperationTest.java
+++ syncany-lib/src/test/java/org/syncany/tests/operations/PluginOperationTest.java
@@ -20,10 +20,14 @@ package org.syncany.tests.operations;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.util.List;
 
 import org.junit.Test;
+import org.syncany.config.UserConfig;
 import org.syncany.operations.plugin.ExtendedPluginInfo;
 import org.syncany.operations.plugin.PluginOperationOptions;
 import org.syncany.operations.plugin.PluginOperationOptions.PluginAction;
@@ -35,8 +39,11 @@ import org.syncany.plugins.Plugins;
 import org.syncany.plugins.local.LocalConnection;
 import org.syncany.tests.util.TestClient;
 import org.syncany.tests.util.TestConfigUtil;
+import org.syncany.tests.util.TestFileUtil;
+import org.syncany.util.EnvironmentUtil;
 
 public class PluginOperationTest {
+	
 	@Test
 	public void testPluginListLocalOnly() throws Exception {
 		// Setup
@@ -119,6 +126,52 @@ public class PluginOperationTest {
 		
 		// Tear down
 		client.deleteTestData();	
+	}
+	
+	@Test
+	public void testPluginInstallAndPluginRemove() throws Exception {
+		// Test the installation of FTP plugin
+		
+		if (EnvironmentUtil.isWindows()) {
+			// Test is Unix-specific.
+			return;
+		}
+		
+		// Set the directory for the global config
+		Field userAppDirUnix = UserConfig.class.getDeclaredField("USER_APP_DIR_UNIX_LIKE");
+		userAppDirUnix.setAccessible(true);
+		File configDir = TestFileUtil.createTempDirectoryInSystemTemp();
+		userAppDirUnix.set(null, configDir);
+		
+		// Forget the current global config
+		Field userConfigDir = UserConfig.class.getDeclaredField("userConfigDir");
+		userConfigDir.setAccessible(true);
+		userConfigDir.set(null, null);
+		
+		// Reinitialize global config
+		UserConfig.init();
+		
+		// Setup
+		LocalConnection testConnection = (LocalConnection) TestConfigUtil.createTestLocalConnection();
+		TestClient client = new TestClient("A", testConnection);
+
+		PluginOperationOptions pluginOptions = new PluginOperationOptions();
+		pluginOptions.setAction(PluginAction.INSTALL);
+		pluginOptions.setPluginId("ftp");
+		
+		PluginOperationResult pluginResult = client.plugin(pluginOptions);		
+		
+		assertNotNull(pluginResult);
+		assertEquals(PluginResultCode.OK, pluginResult.getResultCode());
+		// Only one file should be in here: the jar for ftp.
+		String[] test = (new File(configDir, "plugins/lib/")).list();
+		assertTrue((new File(configDir, "plugins/lib/")).list().length == 1);
+		
+		
+		// Tear down
+		client.deleteTestData();	
+		TestFileUtil.deleteDirectory(configDir);
+		System.setProperty("user.home", "/tmp");
 	}
 	
 }
