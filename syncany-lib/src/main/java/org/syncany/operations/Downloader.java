32cf5c0b650cfe1065f29518648f88ff5d8c6d2f
Philipp Heckel <philipp.heckel@gmail.com>
2014-09-10 22:37:49 +0200

    Delete multichunk from cache if decryption failed. Related to #59

--- syncany-lib/src/main/java/org/syncany/operations/Downloader.java
+++ syncany-lib/src/main/java/org/syncany/operations/Downloader.java
@@ -91,7 +91,7 @@ public class Downloader {
 					logger.log(Level.FINE, "    -> FAILED: Decryption/extraction of multichunk failed, deleting " + multiChunkId + " ...");
 					localDecryptedMultiChunkFile.delete();
 					
-					throw e;
+					throw new IOException("Multichunk decryption/extraction failed. The multichunk might have been tampered with!", e);
 				}
 				finally {
 					logger.log(Level.FINE, "  + Locally deleting multichunk " + multiChunkId + " ...");
