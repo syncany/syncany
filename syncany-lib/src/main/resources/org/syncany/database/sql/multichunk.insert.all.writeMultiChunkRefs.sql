919c42aa6edddd7b15f9f4f3a867b3c4d6579f27
Philipp Heckel <philipp.heckel@gmail.com>
2014-09-29 21:12:54 +0200

    Different approach to resolving issue #227

--- syncany-lib/src/main/resources/org/syncany/database/sql/multichunk.insert.all.writeMultiChunkRefs.sql
+++ syncany-lib/src/main/resources/org/syncany/database/sql/multichunk.insert.all.writeMultiChunkRefs.sql
@@ -1,1 +1,7 @@
-insert into multichunk_chunk (multichunk_id, chunk_checksum) values (?, ?)
+merge into multichunk_chunk as multichunk_chunk_target
+using (values(?, ?)) as multichunk_chunk_ref(multichunk_id, chunk_checksum)
+on (
+	multichunk_chunk_target.multichunk_id = multichunk_chunk_ref.multichunk_id
+	and multichunk_chunk_target.chunk_checksum = multichunk_chunk_ref.chunk_checksum
+)
+when not matched then insert (multichunk_id, chunk_checksum) values (multichunk_chunk_ref.multichunk_id, multichunk_chunk_ref.chunk_checksum)
