81918355a4aff5e34f55d6d0175ddf18f2fcc928
wiencek <vwiencek@gmail.com>
2014-10-02 14:43:47 +0200

    ...

--- syncany-lib/src/main/java/org/syncany/operations/daemon/messages/events/UpStartSyncExternalEvent.java
+++ syncany-lib/src/main/java/org/syncany/operations/daemon/messages/events/UpStartSyncExternalEvent.java
@@ -18,6 +18,10 @@
 package org.syncany.operations.daemon.messages.events;
 
 public class UpStartSyncExternalEvent extends SyncExternalEvent {
+	public UpStartSyncExternalEvent(){
+		// no-arg constructor
+	}
+
 	public UpStartSyncExternalEvent(String root) {
 		super(root);
 	}
