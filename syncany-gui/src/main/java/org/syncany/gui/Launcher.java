7ff912fe48c7b344c1b51f1a71d66143901d6987
wiencek <vwiencek@gmail.com>
2014-10-03 17:31:24 +0200

    add guioperation

--- syncany-gui/src/main/java/org/syncany/gui/Launcher.java
+++ syncany-gui/src/main/java/org/syncany/gui/Launcher.java
@@ -17,6 +17,8 @@
  */
 package org.syncany.gui;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import org.eclipse.swt.widgets.Display;
@@ -24,6 +26,9 @@ import org.syncany.config.Logging;
 import org.syncany.config.UserConfig;
 import org.syncany.gui.util.I18n;
 import org.syncany.gui.util.SWTResourceManager;
+import org.syncany.operations.daemon.DaemonOperation;
+import org.syncany.util.EnvironmentUtil;
+import org.syncany.util.PidFileUtil;
 
 /**
  * @author Vincent Wiencek <vwiencek@gmail.com>
@@ -32,13 +37,21 @@ import org.syncany.gui.util.SWTResourceManager;
 public class Launcher {
 	private static final Logger log = Logger.getLogger(Launcher.class.getSimpleName());
 	
-	private static MainGUI window;
+	public static MainGUI window;
 
 	static {
 		Logging.init();
+		//Logging.disableLogging();
 	}
 
 	public static void main(String[] args) {
+		try {
+			startDeamon();
+		}
+		catch (IOException | InterruptedException e) {
+			log.warning("Unable to start daemon");
+		}
+
 		startApplication();
 	}
 
@@ -48,6 +61,15 @@ public class Launcher {
 	}
 
 	public static void stopApplication() {
+		try {
+			shutDownDaemon();
+		}
+		catch (IOException e) {
+			log.warning("Unable to stop daemon: " + e);
+		}
+		catch (InterruptedException e) {
+			log.warning("Unable to stop daemon: " + e);
+		}
 		stopGUI();
 		System.exit(0);
 	}
@@ -56,9 +78,46 @@ public class Launcher {
 		window.dispose();
 	}
 
+	public static void startDeamon() throws IOException, InterruptedException {
+		File daemonPidFile = new File(UserConfig.getUserConfigDir(), DaemonOperation.PID_FILE);
+
+		boolean daemonRunning = PidFileUtil.isProcessRunning(daemonPidFile);
+
+		if (!daemonRunning){
+			String executable = null;
+			if (EnvironmentUtil.isMaxOsX()){
+				executable = "/usr/local/bin/sy";
+			}
+			else if (EnvironmentUtil.isWindows()){
+				executable = "c:\\windows\\sy.bat";
+			}
+			Process daemonProceee = new ProcessBuilder(executable, "daemon", "start").start();
+			daemonProceee.waitFor();
+		}
+	}
+	
+	public static void shutDownDaemon() throws IOException, InterruptedException {
+		File daemonPidFile = new File(UserConfig.getUserConfigDir(), DaemonOperation.PID_FILE);
+
+		boolean daemonRunning = PidFileUtil.isProcessRunning(daemonPidFile);
+
+		if (daemonRunning){
+			String executable = null;
+			if (EnvironmentUtil.isMaxOsX()){
+				executable = "/usr/local/bin/sy";
+			}
+			else if (EnvironmentUtil.isWindows()){
+				executable = "c:\\windows\\sy.bat";
+			}
+			Process daemonProceee = new ProcessBuilder(executable, "daemon", "stop").start();
+			daemonProceee.waitFor();
+		}
+	}
+	
 	private static void startGUI() {
 		Display.setAppName("Syncany");
-
+		Display.setAppVersion("1.0");
+		
 		// Register messages bundles
 		I18n.registerBundleName("i18n/messages");
 		I18n.registerBundleFilter("plugin_messages*");
