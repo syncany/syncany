c3f3cc18b4eb1019bb7eec9438f053683c1fad96
wiencek <vwiencek@gmail.com>
2014-10-03 17:33:08 +0200

    add guiCommand

---
+++ syncany-cli/src/main/java/org/syncany/cli/GuiCommand.java
@@ -0,0 +1,61 @@
+/*
+ * Syncany, www.syncany.org
+ * Copyright (C) 2011-2014 Philipp C. Heckel <philipp.heckel@gmail.com> 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.syncany.cli;
+
+import org.syncany.operations.Operation;
+import org.syncany.operations.OperationOptions;
+import org.syncany.operations.OperationResult;
+
+import com.sun.corba.se.spi.legacy.connection.GetEndPointInfoAgainException;
+
+public class GuiCommand extends Command {
+	@Override
+	public CommandScope getRequiredCommandScope() {	
+		return CommandScope.ANY;
+	}
+
+	@Override
+	public boolean canExecuteInDaemonScope() {
+		return false;
+	}
+
+	@Override
+	public int execute(String[] operationArgs) throws Exception {
+		String guiOperationClassName = "org.syncany.operations.gui.GuiOperation";
+		
+		try{
+			Class c = Class.forName(guiOperationClassName);
+			Operation obj = (Operation)c.newInstance();
+			obj.execute();
+		}
+		catch (Exception e){
+			out.println("Gui plugin should be installed with 'sy plugin install gui'");
+		}
+		return 0;
+	}
+
+	@Override
+	public OperationOptions parseOptions(String[] operationArgs) throws Exception {
+		return null;
+	}
+
+	@Override
+	public void printResults(OperationResult result) {
+		// Nothing.
+	}	
+}
