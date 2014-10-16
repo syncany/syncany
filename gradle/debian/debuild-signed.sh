01c944556d450608f02aeb705e86b448182f7807
Philipp Heckel <philipp.heckel@gmail.com>
2014-10-17 01:34:49 +0200

    Make debuild-signed.sh reusable

--- gradle/debian/debuild-signed.sh
+++ gradle/debian/debuild-signed.sh
@@ -3,7 +3,15 @@
 SIGNKEYID="651D12BD"
 
 SCRIPTDIR="$( cd "$( dirname "$0" )" && pwd )"
-REPODIR="$SCRIPTDIR/../.."
+
+if [ ! -f "$1" -o ! -d "$2" -o ! -d "$3" ]; then
+	echo "Syntax: $0 <encrypted-key-file> <build-dir> <deb-dir>"
+	exit 1
+fi
+
+ENCRYPTED_KEYFILE="$1"
+BUILDDIR="$2"
+DEBBUILDDIR="$3"
 
 if [ -n "$TRAVIS_PULL_REQUEST" -a "$TRAVIS_PULL_REQUEST" != "false" ]; then
 	echo "NOTE: Skipping GPG stuff. This job is a PULL REQUEST."
@@ -15,55 +23,54 @@ if [ "$SYNCANY_GPG_KEY_WRAP" == "" -o "$SYNCANY_GPG_KEY_PRIV" == "" ]; then
 	exit 1
 fi
 
-mkdir -p $REPODIR/build/debian/gpg
-chmod 700 $REPODIR/build/debian/gpg
+mkdir -p $BUILDDIR/gpg
+chmod 700 $BUILDDIR/gpg
 
 # Write keys to temporary files (yes, this is unsafe, but there is no other way!)
-echo $SYNCANY_GPG_KEY_WRAP > $REPODIR/build/debian/gpg/key-wrap
-echo $SYNCANY_GPG_KEY_PRIV > $REPODIR/build/debian/gpg/key-priv
+echo $SYNCANY_GPG_KEY_WRAP > $BUILDDIR/gpg/key-wrap
+echo $SYNCANY_GPG_KEY_PRIV > $BUILDDIR/gpg/key-priv
 
 # Decrypt signing key
 echo -n "Decrypting signing key ... "
-cat $REPODIR/gradle/debian/gpg/syncany-team.asc.aes256 | gpg -d --no-tty --no-use-agent --passphrase-file=$REPODIR/build/debian/gpg/key-wrap > $REPODIR/build/debian/gpg/syncany-team.asc
+cat $ENCRYPTED_KEYFILE | gpg -d --no-tty --no-use-agent --passphrase-file=$BUILDDIR/gpg/key-wrap > $BUILDDIR/gpg/syncany-team.asc
 
-if [ ! -s "$REPODIR/build/debian/gpg/syncany-team.asc" ]; then
+if [ ! -s "$BUILDDIR/gpg/syncany-team.asc" ]; then
 	echo "FAILED. EXITING"
 	
-	rm -rf $REPODIR/build/debian/gpg
+	rm -rf $BUILDDIR/gpg
 	exit 1
 else
-	rm $REPODIR/build/debian/gpg/key-wrap
+	rm $BUILDDIR/gpg/key-wrap
 	echo "OK"
 fi
 
 # Import to keyring
 echo -n "Importing signing key to keyring ... "
-gpg --homedir=$REPODIR/build/debian/gpg --no-tty --no-use-agent --import $REPODIR/build/debian/gpg/syncany-team.asc
+gpg --homedir=$BUILDDIR/gpg --no-tty --no-use-agent --import $BUILDDIR/gpg/syncany-team.asc
 
 if [ $? -ne 0 ]; then
 	echo "FAILED. EXITING"
 	
-	rm -rf $REPODIR/build/debian/gpg
+	rm -rf $BUILDDIR/gpg
 	exit 1
 else
-	rm $REPODIR/build/debian/gpg/syncany-team.asc
+	rm $BUILDDIR/gpg/syncany-team.asc
 	echo "OK"
 fi
 
 # Running debuild
 echo -n "Running 'debuild' ... " 
-cd $REPODIR/build/debian/syncany
-debuild -k$SIGNKEYID -S -p"gpg --homedir=$REPODIR/build/debian/gpg --no-tty --no-use-agent --passphrase-file=$REPODIR/build/debian/gpg/key-priv"
+cd $DEBBUILDDIR
+debuild -k$SIGNKEYID -S -p"gpg --homedir=$BUILDDIR/gpg --no-tty --no-use-agent --passphrase-file=$BUILDDIR/gpg/key-priv"
 
 if [ $? -ne 0 ]; then
 	echo "FAILED. EXITING"
 	
-	rm -rf $REPODIR/build/debian/gpg
+	rm -rf $BUILDDIR/gpg
 	exit 1
 else
-	rm $REPODIR/build/debian/gpg/key-priv
+	rm $BUILDDIR/gpg/key-priv
 	echo "OK"
 fi
 
-rm -rf $REPODIR/build/debian/gpg
-
+rm -rf $BUILDDIR/gpg
