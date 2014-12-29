a598d55e08644b70fae8300bfe75be99ab0efac8
MrHug <s.hugtenburg@gmail.com>
2014-12-29 12:50:49 +0100

    Fixed copyright notices locally

--- syncany-lib/src/main/java/org/syncany/config/LogFormatter.java
+++ syncany-lib/src/main/java/org/syncany/config/LogFormatter.java
@@ -1,6 +1,6 @@
 /*
  * Syncany, www.syncany.org
- * Copyright (C) 2011-2014 Philipp C. Heckel <philipp.heckel@gmail.com>
+ * Copyright (C) 2011-2015 Philipp C. Heckel <philipp.heckel@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
