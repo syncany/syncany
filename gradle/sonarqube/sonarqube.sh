876ddc06b98d15b03181d543cc1e5515c823f4d0
MrHug <s.hugtenburg@gmail.com>
2015-03-01 18:27:26 +0100

    Ensure Sonar reuses reports
    As requested in #395, this will get the execution time for Sonar down to a
    minute or so and still only run when pushing to `develop`/`master`

--- gradle/sonarqube/sonarqube.sh
+++ gradle/sonarqube/sonarqube.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 if [ -n "$TRAVIS_PULL_REQUEST" -a "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo "NOTE: Skipping SonarQube analysis. This job is a PULL REQUEST."
 	exit 0
 fi
 
-./gradlew sonarRunner
+echo "sonarRunner"
 
