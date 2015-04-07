2de0304bb400cb12a70020ee684aadf0a3d4f0bf
Philipp Heckel <philipp.heckel@gmail.com>
2015-04-07 23:39:43 +0100

    JavaDocing packages

---
+++ syncany-lib/src/main/java/org/syncany/crypto/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes for encryption/decryption of files and byte
+ * streams. It implements the application's crypto concept.
+ * 
+ * @see <a href="http://syncany.readthedocs.org/en/latest/security.html">Syncany security concepts</a>
+ */
+package org.syncany.crypto;
