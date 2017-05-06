5564e25f10cd60fb177e7ab0df700872cff79db1
Pim Otte <otte.pim@gmail.com>
2017-05-06 16:12:30 +0200

    Writing tests for databaseversion comparator and rewriting it a little

--- syncany-lib/src/main/java/org/syncany/operations/down/DatabaseVersionHeaderComparator.java
+++ syncany-lib/src/main/java/org/syncany/operations/down/DatabaseVersionHeaderComparator.java
@@ -25,13 +25,18 @@ import org.syncany.database.VectorClock.VectorClockComparison;
 
 /**
  * Comparator to be used when comparing {@link DatabaseVersionHeader}s. The comparison precedence
- * is as follows:
+ * is as follows, if the comparator is time sensitive.
  * 
  * <ol>
  *  <li>Comparison by {@link VectorClock}</li>
  *  <li>Comparison by timestamp/date</li>
  *  <li>Comparison by name of the client</li>
  * </ol>
+ *
+ * If the comparator is not time sensitive, the ordering is specified solely by the {@link VectorClock}s.
+ * Larger and smaller {@link VectorClock}s result in larger, respectively smaller {@link DatabaseVersionHeader}s.
+ * Equal and simultaneous {@link VectorClock}s result in equal {@link DatabaseVersionHeader}s. Note that in
+ * this case the name of the client is not used either.
  * 
  * @author Philipp C. Heckel <philipp.heckel@gmail.com>
  */
@@ -45,48 +50,42 @@ public class DatabaseVersionHeaderComparator implements Comparator<DatabaseVersi
 	/**
 	 * Compares the two given database versions headers and returns -1, 0 or 1 depending on
 	 * which header is considered larger. See {@link DatabaseVersionHeaderComparator class description}
-	 * for details regarding the precedence.  
+	 * for details regarding the precedence.
+	 *
+	 * @return -1 if dbvh1 is smaller than dbvh2, 0 if they are equal, 1 if dbvh1 is greater than dbvh2
 	 */
 	@Override
-	public int compare(DatabaseVersionHeader o1, DatabaseVersionHeader o2) {
-		return compareByVectorClock(o1, o2);
+	public int compare(DatabaseVersionHeader dbvh1, DatabaseVersionHeader dbvh2) {
+		return compareByVectorClock(dbvh1, dbvh2);
 	}
 
-	private int compareByVectorClock(DatabaseVersionHeader o1, DatabaseVersionHeader o2) {
-		VectorClockComparison vectorClockComparison = VectorClock.compare(o1.getVectorClock(), o2.getVectorClock());
+	private int compareByVectorClock(DatabaseVersionHeader dbvh1, DatabaseVersionHeader dbvh2) {
+		VectorClockComparison vectorClockComparison = VectorClock.compare(dbvh1.getVectorClock(), dbvh2.getVectorClock());
 		
-		if (vectorClockComparison == VectorClockComparison.SIMULTANEOUS) {
+		if (vectorClockComparison == VectorClockComparison.SIMULTANEOUS || vectorClockComparison == VectorClockComparison.EQUAL) {
 			if (considerTime) {
-				return compareByTimestamp(o1, o2);
+				return compareByTimestamp(dbvh1, dbvh2);
 			}
 			else {
 				return 0;
 			}
 		}
-		
-		if (vectorClockComparison == VectorClockComparison.EQUAL) {
-			return compareByTimestamp(o1, o2);
-		}		
-		else if (vectorClockComparison == VectorClockComparison.SMALLER) {
-			return -1;
-		}
-		else {
-			return 1;
-		}
+
+		return vectorClockComparison == VectorClockComparison.SMALLER ? -1 : 1;
 	}
 
-	private int compareByTimestamp(DatabaseVersionHeader o1, DatabaseVersionHeader o2) {
-		int timestampComparison = Long.compare(o1.getDate().getTime(), o2.getDate().getTime());
+	private int compareByTimestamp(DatabaseVersionHeader dbvh1, DatabaseVersionHeader dbvh2) {
+		int timestampComparison = Long.compare(dbvh1.getDate().getTime(), dbvh2.getDate().getTime());
 
 		if (timestampComparison == 0) {
-			return compareByClientName(o1, o2);
+			return compareByClientName(dbvh1, dbvh2);
 		}
 		else {
 			return timestampComparison;
 		}
 	}
 
-	private int compareByClientName(DatabaseVersionHeader o1, DatabaseVersionHeader o2) {
-		return o1.getClient().compareTo(o2.getClient());
+	private int compareByClientName(DatabaseVersionHeader dbvh1, DatabaseVersionHeader dbvh2) {
+		return dbvh1.getClient().compareTo(dbvh2.getClient());
 	}
 }
